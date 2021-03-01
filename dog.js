86224564e424f456bc410840aa95f9bc781d89da
roberterpi <robertpopct@gmail.com>
2021-03-01 16:31:23 +0200

    added line 14 in html and new js codeline from 24-36

--- dog.js
+++ dog.js
@@ -1,8 +1,8 @@
+/* ajax and json exercise 
 const BREEDS_URL = "https://dog.ceo/api/breeds/image/random";
 
-
 function addDog () {
-
+  //show loading spinner
   fetch(BREEDS_URL)
     .then(function(response) {
       return response.json();
@@ -13,7 +13,24 @@ function addDog () {
       img.alt = 'cute dog';
   
       document.querySelector('.dog').appendChild(img);
+      //stop loading
     })
 }
 
 document.querySelector('.add-dog').addEventListener('click', addDog);
+*/
+
+
+const BREEDS_URL = 'https://dog.ceo/api/breeds/list/all';
+
+fetch(BREEDS_URL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    //console.log(data);
+    const breedsObject = data.message;
+    const breedsArray = Object.keys(breedsObject);
+    //another way of doing it
+    //console.log(Object.keys(data.message));
+  })
